d4303bc1002ba6db245aa41eef2542325f28a378
Simon-Bertram <simonbertram@hotmail.com>
2024-04-18 14:45:18 +0100

    Create components and add assets

---
+++ src/assets/index.js
@@ -0,0 +1,28 @@
+import github from "./github.png"
+
+import css from "./tech/css.png"
+import docker from "./tech/docker.png"
+import git from "./tech/git.png"
+import html from "./tech/html.png"
+import javascript from "./tech/javascript.png"
+import mongodb from "./tech/mongodb.png"
+import nodejs from "./tech/nodejs.png"
+import reactjs from "./tech/reactjs.png"
+import redux from "./tech/redux.png"
+import tailwind from "./tech/tailwind.png"
+import typescript from "./tech/typescript.png"
+
+export {
+  css,
+  docker,
+  git,
+  github,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+}
---
+++ src/constants/index.js
@@ -0,0 +1,61 @@
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+]
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+]

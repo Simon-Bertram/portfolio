1f6f7b733bd7a66b490a01d512636e349f10e0b4
Simon-Bertram <simonbertram@hotmail.com>
2024-04-19 15:26:04 +0100

    Add Hero background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,17 @@
+import heroBackgroundPNG from "../assets/herobg.png"
+import heroBackgroundWEBP from "../assets/herobg.webp"
+
 const Hero = () => {
   return (
-    <div>
-      <h1>Hero</h1>
+    <div className="bg-cover bg-center h-screen">
+      <picture>
+        <source srcSet={heroBackgroundWEBP} type="image/webp" />
+        <img
+          className="bg-image w-full h-full object-cover"
+          src={heroBackgroundPNG}
+          alt="hero background"
+        />
+      </picture>
     </div>
   )
 }
